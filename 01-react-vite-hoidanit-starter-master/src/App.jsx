5f4b29506e35b086cb13c255a464d63747c30ee6
ocki26 <ocki2zz65@gmail.com>
2025-05-02 22:56:22 +0700

    video 27 react

--- 01-react-vite-hoidanit-starter-master/src/App.jsx
+++ 01-react-vite-hoidanit-starter-master/src/App.jsx
@@ -3,8 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
+
+  // function myFunction() {
+  //   console.log("Hello World");
+  // }
+  const myFunction = () => {
+    console.log("Hello World");
+  };
+  myFunction();
 
   return (
     <>
@@ -30,6 +38,6 @@ function App() {
       </p>
     </>
   );
-}
+};
 
 export default App;
