afbd92c52dda686057e76095808712a46feeddd3
ocki26 <ocki2zz65@gmail.com>
2025-05-04 23:14:28 +0700

    video 31

--- 01-react-vite-hoidanit-starter-master/src/componets/learn/MyComponent.jsx
+++ 01-react-vite-hoidanit-starter-master/src/componets/learn/MyComponent.jsx
@@ -1,8 +1,14 @@
 import "./Style.css";
 const MyComponent = () => {
+  // const hoiDanIt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const hoiDanIt = {
+    name: "hoidanit",
+    age: 18,
+  };
   return (
     <>
-      <div>hoi dan truong</div>
+      <div>{console.log("banh my")}</div>
+      <div>{JSON.stringify(hoiDanIt)}hoi dan truong</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         hoi dan it
       </div>
