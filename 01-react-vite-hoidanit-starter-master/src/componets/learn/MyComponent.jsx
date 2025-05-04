1b35fb74237ead07983d16265216a4cbf18aa781
ocki26 <ocki2zz65@gmail.com>
2025-05-04 22:49:29 +0700

    video30 JSX

--- 01-react-vite-hoidanit-starter-master/src/componets/learn/MyComponent.jsx
+++ 01-react-vite-hoidanit-starter-master/src/componets/learn/MyComponent.jsx
@@ -1,4 +1,12 @@
+import "./Style.css";
 const MyComponent = () => {
-  return <div>hoi dan truong</div>;
+  return (
+    <>
+      <div>hoi dan truong</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        hoi dan it
+      </div>
+    </>
+  );
 };
 export default MyComponent;
