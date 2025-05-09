afb364bd24335be9f07bc5df8e75ff398964b504
ocki26 <ocki2zz65@gmail.com>
2025-05-09 22:53:35 +0700

    video36

--- 01-react-vite-hoidanit-starter-master/src/componets/todo/TodoData.jsx
+++ 01-react-vite-hoidanit-starter-master/src/componets/todo/TodoData.jsx
@@ -3,14 +3,12 @@ const TodoData = (props) => {
   //hoac nhu kieu nay
   //const name = props.name;
   return (
-    console.log("check props ==>", props),
-    (
-      <div className="to-do-data">
-        <div>my name is {name}</div>
-        <div>learning react</div>
-        <div>watching on youtube</div>
-      </div>
-    )
+    // console.log("check props ==>", props),
+    <div className="to-do-data">
+      <div>my name is {name}</div>
+      <div>learning react</div>
+      <div>watching on youtube</div>
+    </div>
   );
 };
 export default TodoData;
--- 01-react-vite-hoidanit-starter-master/src/componets/todo/TodoNew.jsx
+++ 01-react-vite-hoidanit-starter-master/src/componets/todo/TodoNew.jsx
@@ -1,11 +1,27 @@
 const TodoNew = (props) => {
-  console.log("check props ==>", props);
+  // console.log("check props ==>", props);
   const { AddNewToDo } = props;
   AddNewToDo("hoidanit");
+  const Handleclick = () => {
+    alert("ban da click");
+  };
+  const Handleonchange = (name) => {
+    console.log("Handleonchange", name);
+  };
   return (
     <div className="to-do-new">
-      <input className="input_todo" type="text" />
-      <button className="button_add">add</button>
+      <input
+        className="input_todo"
+        type="text"
+        onChange={(event) => Handleonchange(event.target.value)}
+      />
+      <button
+        className="button_add"
+        style={{ cursor: "pointer" }}
+        onClick={Handleclick}
+      >
+        add
+      </button>
     </div>
   );
 };
